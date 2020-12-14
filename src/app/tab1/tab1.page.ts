5cde15d9890d77e41e8149a02996f8299646173f
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2020-12-14 23:24:12 +0700

    versi web

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,51 +7,35 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  gender: string = 'male';
+  age: string = 'adult';
+  fluid: string = 'Normal saline';
   weight: number;
-  weightx: number;
-  height: number;
-  heightx: number;
-  satuan1: string = 'kg';
-  satuan2: string = 'cm';
-  time: number;
-  fluid: number;
-  child: boolean = false;
-  burn: number;
-  note1: string;
-  note2: string;
-  note3: string;
-  note4: string;
-  note5: string;
-  note6: string;
-  note7: string;
+  sodium: number;
+  correction: number;
+  fluiddrate: string;
 
   constructor(public alertController: AlertController) {
   }
 
+  check1(event){
+    this.gender = event.target.value;
+  }
+  check2(event){
+    this.age = event.target.value;
+  }
+  check3(event){
+    this.fluid = event.target.value;
+  }
+
   clear(){
     this.weight = null;
-    this.height = null;
-    this.time = null;
-    this.fluid = null;
-    this.burn = null;
-    this.child = false;
-    this.note1 = null;
-    this.note2 = null;
-    this.note3 = null;
-    this.note4 = null;
-    this.note5 = null;
-    this.note6 = null;
-    this.note7 = null;
+    this.sodium = null;
+    this.correction = null;
+    this.fluiddrate = null;
   }
 
-  checksatuan1(event){
-    this.satuan1 = event.target.value;
-  }
-  checksatuan2(event){
-    this.satuan2 = event.target.value;
-  }
-
-  async presentAlert() {
+  async presentAlert1() {
     const alert = await this.alertController.create({
       header: 'Missing Value',
       message: 'Please input all the required values!',
@@ -62,7 +46,23 @@ export class Tab1Page {
   async presentAlert2() {
     const alert = await this.alertController.create({
       header: 'Wrong Value',
-      message: 'Time since burn must be between 0-23 hours!',
+      message: 'Sodium concentration range: 100-140 mEq/L',
+      buttons: ['Okay']
+    });
+    await alert.present();
+  }
+  async presentAlert3() {
+    const alert = await this.alertController.create({
+      header: 'Wrong Value',
+      message: 'Sodium correction rate range: 0-1 mEq/L/hour',
+      buttons: ['Okay']
+    });
+    await alert.present();
+  }
+  async presentAlert4() {
+    const alert = await this.alertController.create({
+      header: 'Wrong Fluid',
+      message: 'You need another fluid. Ringers lactate concentration of Na is 130 mEq/L!',
       buttons: ['Okay']
     });
     await alert.present();
@@ -71,7 +71,7 @@ export class Tab1Page {
     var value4 = "" + Math.round(value);
     var bonus2 = value4.length + 1;
     var bonus = 0;
-    if (value < 100){bonus=bonus+1};
+    if (value < 100000){bonus=bonus+1};
     if (value < 10){bonus=bonus+1};
     if (value < 1){bonus=bonus+1};
     if (value < 0.1){bonus=bonus+1};
@@ -85,88 +85,25 @@ export class Tab1Page {
     return whole3;
   }
   calculate(){
-    if (this.height == null || this.weight == null || this.time == null || this.fluid == null || this.burn == null || 
-      this.height < 0 || this.weight < 0 || this.time < 0 || this.fluid < 0 || this.burn < 0){
-      this.presentAlert()
-    } else if (this.time >= 24) {this.presentAlert2()}
+    if (this.weight == null || this.sodium == null || this.correction == null){this.presentAlert1()} 
+    else if (this.sodium <100 || this.sodium > 140) {this.presentAlert2()}
+    else if (this.correction <=0 || this.correction > 1) {this.presentAlert3()}
+    else if (this.sodium >= 130 && this.fluid == 'Ringers lactate'){this.presentAlert4()}
     else {
-      if (this.satuan1 == 'kg'){
-        this.weightx = this.weight;
-      } else if (this.satuan1 == 'lb'){
-        this.weightx = this.weight*0.45359237;
-      }
-      if (this.satuan2 == 'cm'){
-        this.heightx = this.height;
-      } else if (this.satuan2 == 'ft'){
-        this.heightx = this.height*30.48;
-      }
-      var parkland = 4 * this.burn * this.weightx;
-      this.note1 = 'The fluid requirements from the first 24 hours from time of burn are ' + this.roundoff(parkland) + ' mL. (4 mL x kg body weight x % burn area)'
-      //kasih catatan cairan yang diperlukan 24 jam pertama
-      var halfparkland = parkland / 2;
-      this.note2 = this.roundoff(halfparkland) + ' mL are given in the first 8 hours and ' + this.roundoff(halfparkland) + ' mL are given in the second 16 hours.'
-      //kasih catatan cairan yang dibutuhkan 8 jam dan 16 jam
-      this.note3 = 'The patient had been given ' + this.roundoff(this.fluid) + ' mL in the first ' + this.time + ' hours from the time of burn.'
-        if (this.time < 8){
-          if (this.fluid > halfparkland){
-            //kasih catatan cairan yang masuk sudah cukup melebihi yang dibutuhkan dalam 8 jam pertama
-            if (this.fluid > parkland) {
-              this.note4 = 'Fluid that had been given are more than the required fluid in the first 24 hours.'
-              //kasih catatan cairan yang masuk sudah melebihi yang dibutuhkan dalam 24 jam pertama
-            } else {
-              var first24hours = (parkland-this.fluid)/(24-this.time);
-              this.note4 = 'Fluid that had been given are more than the required fluid in the first 8 hours. For the next '
-              + (24-this.time) + ' hours, the patient needs ' + this.roundoff(parkland-this.fluid) + ' mL (' + this.roundoff(first24hours)
-              + ' mL/hour).';
-              //kasih catatan cairan yang perlu masuk sisanya
-            }
-          } else {
-            var first8hours = (halfparkland - this.fluid)/(8-this.time);
-            var first16hours = halfparkland/16;
-            this.note4 = 'The patient needs ' + this.roundoff(halfparkland-this.fluid) + ' mL in the first ' + (8-this.time) + ' hours ('
-            + this.roundoff(first8hours) + ' mL/hour) and ' + this.roundoff(halfparkland) + ' mL in the second 16 hours (' + this.roundoff(first16hours) + ' mL/hour).'
-            //kasih catatan cairan yang perlu masuk 8 jam pertama dan 16 jam pertama per hour 
-          }
-        } else {
-          if (this.fluid > parkland){
-            this.note4 = 'Fluid that had been given are more than the required fluid in the first 24 hours.'
-            //kasih catatan cairan yang masuk sudah melebihi yang dibutuhkan dalam 24 jam pertama
-          } else {
-            var first24hours = (parkland-this.fluid)/(24-this.time);
-            this.note4 = 'For the next '
-              + (24-this.time) + ' hours, the patient needs ' + this.roundoff(parkland-this.fluid) + ' mL (' + this.roundoff(first24hours)
-              + ' mL/hour).';
-          }
-        }
-      var bsa = Math.sqrt(this.heightx *this.weightx/3600)
-      this.note5 = 'Body surface area (BSA) is ' + this.roundoff(bsa) + ' square meter (Mosteller formula).'
-      if (this.child == false){
-          this.note7 = 'Target urine output for adult is 0.5 - 1.0 mL/kg body weight/hour.'
-          var maintenance = 1500 * bsa;
-          var maintenancerate = maintenance/24;
-          var evaporative = (25 + this.burn) * bsa;
-          var total = maintenancerate + evaporative;
-        } else {
-          this.note7 = 'Target urine output for children is 1.0 - 2.0 mL/kg body weight/hour.'
-          if (this.weightx > 20){
-            var maintenance = 1500 + (this.weightx-20)*20;
-            var maintenancerate = maintenance/24;
-            var evaporative = (25 + this.burn) * bsa
-            var total = maintenancerate + evaporative;
-          } else if (this.weightx > 10){
-            var maintenance = 1000 + (this.weightx-10)*50;
-            var maintenancerate = maintenance/24;
-            var evaporative = (35 + this.burn) * bsa
-            var total = maintenancerate + evaporative;
-          } else {
-            var maintenance = this.weightx*100;
-            var maintenancerate = maintenance/24;
-            var evaporative = (35 + this.burn) * bsa
-            var total = maintenancerate + evaporative;
-          }
-        }  
-        this.note6 = 'Maintenance fluid rate (' + this.roundoff(total) + ' mL/hour) = basal requirements (' + this.roundoff(maintenancerate) + ' mL/hour) + evaporative lossess (' + this.roundoff(evaporative) + ' mL/hour). It is usually given after the first 24 hours but can also be given during resuscitaion period (the first 24 hours), especially in children.';
-      }
+      var TBW: number;
+      if ((this.age == 'child') || (this.age == 'adult' && this.gender == 'male')){TBW = this.weight * 0.6}
+      else if ((this.age == 'adult' && this.gender == 'female') || (this.age == 'elderly' && this.gender == 'male')){TBW = this.weight * 0.5}
+      else if (this.age == 'elderly' && this.gender == 'female'){TBW = this.weight * 0.45}
+      var change: number;
+      var rate: number;
+      var day: number;
+      day = this.correction * 24;
+      if (this.fluid == 'Normal saline'){change = (154-this.sodium)/(TBW+1);}
+      else if (this.fluid == '3% hypertonic saline'){change = (513-this.sodium)/(TBW+1);}
+      else if (this.fluid == '5% hypertonic saline'){change = (856-this.sodium)/(TBW+1);}
+      else if (this.fluid == 'Ringers lactate'){change = (130-this.sodium)/(TBW+1);}
+      rate = 1000 * this.correction / change;
+      this.fluiddrate = 'For desired correction rate of: '+this.correction+' mEq/L/hour, the fluid correction rate using '+this.fluid+' is '+this.roundoff(rate)+' mL/hour. This will correct sodium by '+this.roundoff(day)+' mEq/L per day.'
     }
-
+  }   
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,28 +6,31 @@ import { AlertController } from '@ionic/angular';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-
 export class Tab2Page {
-  body1: boolean = false;
-  body2: boolean = false;
-  body3: boolean = false;
-  body4: boolean = false;
-  body5: boolean = false;
-  body6: boolean = false;
-  body7: boolean = false;
-  body8: boolean = false;
-  body9: boolean = false;
-  body10: boolean = false;
-  body11: boolean = false;
-  body12: boolean = false;
-  body13: boolean = false;
-  body14: boolean = false;
-  bsa: number = 0;
+  gender: string = 'male';
+  age: string = 'adult';
+  weight: number;
+  sodium: number;
+  correction: number;
+  deficit: string;
 
   constructor(public alertController: AlertController) {
   }
 
-  async presentAlert() {
+  check1(event){
+    this.gender = event.target.value;
+  }
+  check2(event){
+    this.age = event.target.value;
+  }
+  clear(){
+    this.weight = null;
+    this.sodium = null;
+    this.correction = null;
+    this.deficit = null;
+  }
+
+  async presentAlert1() {
     const alert = await this.alertController.create({
       header: 'Missing Value',
       message: 'Please input all the required values!',
@@ -38,54 +41,40 @@ export class Tab2Page {
   async presentAlert2() {
     const alert = await this.alertController.create({
       header: 'Wrong Value',
-      message: 'Time since burn must be between 0-23 hours!',
+      message: 'Current sodium level must be less than desired sodium level!',
       buttons: ['Okay']
     });
     await alert.present();
   }
-  calculate(){
-    this.bsa = 0;
-    if (this.body1 == true){
-      this.bsa   = this.bsa + 4.5;
-    }
-    if (this.body2 == true){
-      this.bsa   = this.bsa + 4.5;
-    }
-    if (this.body3 == true){
-      this.bsa   = this.bsa + 9;
-    }
-    if (this.body4 == true){
-      this.bsa   = this.bsa + 9;
-    }
-    if (this.body5 == true){
-      this.bsa   = this.bsa + 18;
-    }
-    if (this.body6 == true){
-      this.bsa   = this.bsa + 4.5;
-    }
-    if (this.body7 == true){
-      this.bsa   = this.bsa + 4.5;
-    }
-    if (this.body8 == true){
-      this.bsa   = this.bsa + 4.5;
-    }
-    if (this.body9 == true){
-      this.bsa   = this.bsa + 4.5;
-    }
-    if (this.body10 == true){
-      this.bsa   = this.bsa + 9;
-    }
-    if (this.body11 == true){
-      this.bsa   = this.bsa + 9;
-    }
-    if (this.body12 == true){
-      this.bsa   = this.bsa + 9;
-    }
-    if (this.body13 == true){
-      this.bsa   = this.bsa + 9;
-    }
-    if (this.body14 == true){
-      this.bsa   = this.bsa + 1;
-    }
+  
+  roundoff(value){
+    var value4 = "" + Math.round(value);
+    var bonus2 = value4.length + 1;
+    var bonus = 0;
+    if (value < 100000){bonus=bonus+1};
+    if (value < 10){bonus=bonus+1};
+    if (value < 1){bonus=bonus+1};
+    if (value < 0.1){bonus=bonus+1};
+    if (value < 0.01){bonus=bonus+1};
+    if (value < 0.001){bonus=bonus+1};
+    if (value < 0.0001){bonus=bonus+1};
+    bonus2 = bonus2+bonus;
+    var whole = Math.round(value * Math.pow(10, bonus));
+    var whole2 = "" + whole * Math.pow(10, -1*bonus);
+    var whole3 = whole2.substr(0,bonus2);
+    return whole3;
   }
+  calculate(){
+    if (this.weight == null || this.sodium == null || this.correction == null){this.presentAlert1()} 
+    else if (this.sodium > this.correction) {this.presentAlert2()}
+    else {
+      var TBW: number;
+      if ((this.age == 'child') || (this.age == 'adult' && this.gender == 'male')){TBW = this.weight * 0.6}
+      else if ((this.age == 'adult' && this.gender == 'female') || (this.age == 'elderly' && this.gender == 'male')){TBW = this.weight * 0.5}
+      else if (this.age == 'elderly' && this.gender == 'female'){TBW = this.weight * 0.45}
+      var sodium: number;
+      sodium = (this.correction -  this.sodium)*TBW;
+      this.deficit = 'The sodium deficit is: '+this.roundoff(sodium)+' mEq.'
+    }
+  }   
 }
